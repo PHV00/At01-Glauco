accabd73bcfb117add153959ac7884b1a375731d
PHV00 <vitoretipedro@gmail.com>
2026-02-17 15:22:06 -0300

    feat: linked list problem to solve

---
+++ atividades-pedro/atv-linkedlist-01.py
@@ -0,0 +1,3 @@
+# https://www.geeksforgeeks.org/python/python-linked-list/
+# https://www.geeksforgeeks.org/dsa/linked-list-data-structure/
+# https://www.geeksforgeeks.org/dsa/pairwise-swap-elements-of-a-given-linked-list/
