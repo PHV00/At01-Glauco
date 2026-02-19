34555bcdbdc16c4d067b30492362140074b67672
PHV00 <vitoretipedro@gmail.com>
2026-02-18 22:05:22 -0300

    wip: new problens

--- atividades-pedro/atv-linkedlist-01.py
+++ atividades-pedro/atv-linkedlist-01.py
@@ -1,3 +1,16 @@
-# https://www.geeksforgeeks.org/python/python-linked-list/
-# https://www.geeksforgeeks.org/dsa/linked-list-data-structure/
-# https://www.geeksforgeeks.org/dsa/pairwise-swap-elements-of-a-given-linked-list/
+"""
+-------------------------------------------------------------------------------------------------------------------------------------
+Disciplina: Algoritmos Avançados   
+Curso: Engenharia de Software  
+Aluno(a): Pedro Henrique Vitoreti
+Professor: Glauco Scheffel
+-------------------------------------------------------------------------------------------------------------------------------------
+Nome do problema : Union of Arrays with Duplicates
+Link do problema : 
+Plataforma utilizada : GeeksforGeeks 
+Estrutura de dados principal usada :
+Justificativa:
+-------------------------------------------------------------------------------------------------------------------------------------
+"""
+
+# https://www.geeksforgeeks.org/problems/detect-loop-in-linked-list/0?utm_source=chatgpt.com
--- atividades-pedro/atv-stack-01.py
+++ atividades-pedro/atv-stack-01.py
@@ -1,2 +1,23 @@
-# https://www.geeksforgeeks.org/dsa/how-to-identify-and-solve-monotonic-stack-problems/
-# https://www.geeksforgeeks.org/dsa/next-greater-element/
+"""
+-------------------------------------------------------------------------------------------------------------------------------------
+Disciplina: Algoritmos Avançados   
+Curso: Engenharia de Software  
+Aluno(a): Pedro Henrique Vitoreti
+Professor: Glauco Scheffel
+-------------------------------------------------------------------------------------------------------------------------------------
+Nome do problema : Union of Arrays with Duplicates
+Link do problema : https://www.geeksforgeeks.org/dsa/next-greater-element/
+Link Artigo associado ao problema : https://www.geeksforgeeks.org/dsa/how-to-identify-and-solve-monotonic-stack-problems/
+Plataforma utilizada : GeeksforGeeks 
+Estrutura de dados principal usada : 
+Justificativa: 
+-------------------------------------------------------------------------------------------------------------------------------------
+"""
+
+"""
+PROBLEMA:
+
+
+"""
+
+# https://www.geeksforgeeks.org/problems/queue-using-two-stacks/1?utm_source=chatgpt.com
